e0f90c10721a543c3b2108304c7bbe6a6da44164
Fatma Anwar <62119653+Fatma-anwar123@users.noreply.github.com>
2022-06-23 22:15:11 +0200

    Add files via upload

---
+++ A. Watermelon/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{ int input;
+   cin>>input;
+   if(input%2==0%% k>2){
+        cout<<"YES"<<endl;
+
+   }
+   else{
+    cout<<"NO"<<endl;
+   }
+
+}
